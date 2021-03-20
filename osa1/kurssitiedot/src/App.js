5ab6a0247b9eb4e64b12dd9f602d5e04f824c4dd
SSez <>
2021-03-21 01:21:02 +0200

    Update App.js

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -12,18 +12,20 @@ const Part = ({name, count}) => {
 
 const Content = (props) => {
   const p = props.content
-  const listItems = p.map((p) => <Part name={p.name} count={p.exercises} />)
+  const listParts = p.map( (p) => <Part key={p.id} name={p.name} count={p.exercises} /> )
   return(
     <div>
-      {listItems}
+      {listParts}
     </div>
   )
 }
 
 const Total = (props) => {
+  const p = props.count
+  const sum = p.reduce((a, b) =>  a = a + b.exercises, 0)
   return(
     <div>
-      <p>Number of exercises {props.count[0].exercises + props.count[1].exercises + props.count[2].exercises}</p>
+      <p>Number of exercises {sum}</p>
     </div>
   )
 }
@@ -33,14 +35,17 @@ const App = () => {
     name: 'Half Stack application development',
     parts: [
       {
+        id: 1,
         name: 'Fundamentals of React',
         exercises: 10
       },
       {
+        id: 2,
         name: 'Using props to pass data',
         exercises: 7
       },
       {
+        id: 3,
         name: 'State of a component',
         exercises: 14
       }
