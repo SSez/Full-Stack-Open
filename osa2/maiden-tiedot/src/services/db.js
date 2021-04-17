5d1dbadb566a688c1dfdcf344042a431e86370e7
SSez <>
2021-04-17 20:30:56 +0300

    .

--- osa2/maiden-tiedot/src/services/db.js
+++ osa2/maiden-tiedot/src/services/db.js
@@ -5,6 +5,9 @@ const REST_COUNTRY_URL = 'https://restcountries.eu/rest/v2/all'
 const API_URL = "http://api.weatherstack.com/"
 const API_KEY = process.env.REACT_APP_API_KEY
 
+// REACT_APP_API_KEY=a9120f68fc9587d2ec0451ee13510c89 npm start
+// http://api.weatherstack.com/current?access_key=a9120f68fc9587d2ec0451ee13510c89&query=Finland
+
 const getCountries = () => {
   const request = axios.get(REST_COUNTRY_URL)
   return request.then(response => response.data)
