c32811f0468e3aa42c728ffd6a81bfbff7ed87c4
SSez <ZenServices@outlook.com>
2021-05-16 23:45:24 +0300

    Update db.js

--- osa3/puhelinluettelon-backend/models/db.js
+++ osa3/puhelinluettelon-backend/models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator');
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
@@ -28,9 +28,9 @@ const personSchema = new mongoose.Schema({
 
 personSchema.set("toJSON", {
     transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
     },
 })
 
