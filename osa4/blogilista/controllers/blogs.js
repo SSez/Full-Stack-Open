92f7f6311c283dfd74b981a074f20973398d50e9
SSez <ZenServices@outlook.com>
2021-05-24 22:28:29 +0300

    .

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -10,11 +10,17 @@ blogRouter.get('/', async (request, response) => {
 blogRouter.post('/', userExtractor, async (request, response) => {
   const body = request.body
   const user = request.user
+  let likes
+  if(!(body.likes)) {
+      likes = 0
+  } else {
+      likes = body.likes
+  }
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes,
+    likes: likes,
     user: user._id
   })
   const savedBlog = await blog.save()
@@ -48,7 +54,6 @@ blogRouter.delete('/:id', userExtractor, async (request, response) => {
   response.status(204).end()
 })
 
-/*
 blogRouter.put('/:id', userExtractor, async (request, response) => {
   if (!request.params.id) {
     return response.status(400).end()
@@ -62,16 +67,9 @@ blogRouter.put('/:id', userExtractor, async (request, response) => {
   if (findBlog.user._id.toString() !== user.id.toString()) {
     return response.status(401).json({ error: 'Unauthorized' })
   }
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-    user: user._id
-  })
-  await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-  response.json(blog)
+  const blog = request.body
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.status(200).json(updatedBlog.toJSON())
 })
-*/
 
 module.exports = blogRouter
